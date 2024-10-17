0c9eedb9d9f5572bdab691ee860415fe4ecaf3df
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-10-18 00:37:13 +0600

    initialized Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
       },
     },
   },
+
   plugins: [],
 };
 export default config;
